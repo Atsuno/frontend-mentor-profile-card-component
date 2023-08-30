f802482447163f71a5a013be0d1ac70fc180f1bd
Atsuno <achiptsuno@gmil.com>
2023-08-30 11:36:43 +0700

    done main

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        kumbh: ['Kumbh', 'sans-serif'],
+        kumbh: ['Kumbh'],
       },
       colors: {
         primaly: 'hsl(229, 23%, 23%)',
         secondly: 'hsl(0, 0%, 59%)',
         rear: 'hsl(185, 75%, 39%)',
         by: 'hsl(228, 45%, 44%)',
+        thirdary: 'hsl(227, 10%, 46%)',
       },
     },
   },
