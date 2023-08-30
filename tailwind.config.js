9178fe31801f9a1111657743779e1296a124207d
Atsuno <achiptsuno@gmil.com>
2023-08-30 10:46:52 +0700

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kumbh: ['Kumbh', 'sans-serif'],
+      },
+      colors: {
+        primaly: 'hsl(229, 23%, 23%)',
+        secondly: 'hsl(0, 0%, 59%)',
+        rear: 'hsl(185, 75%, 39%)',
+        by: 'hsl(228, 45%, 44%)',
+      },
+    },
   },
   plugins: [],
 }
